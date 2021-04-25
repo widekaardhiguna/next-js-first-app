60fc6c11831786a00d162075e0f28e509a7d3b93
Wideka Ardhiguna <wideka.ardhiguna@jatis.com>
2021-04-25 19:14:54 +0700

    Added index and detail

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a cumque atque quia dignissimos distinctio magnam. Voluptate, laudantium at maxime fuga harum hic adipisci ad iste. Tempore eligendi quidem ad?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit magni ex itaque accusantium veritatis minus veniam neque repellendus cum. Voluptatem distinctio beatae dolor? Corporis labore repellat laudantium, tempore non animi?</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit a cumque atque quia dignissimos distinctio magnam. Voluptate, laudantium at maxime fuga harum hic adipisci ad iste. Tempore eligendi quidem ad?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit magni ex itaque accusantium veritatis minus veniam neque repellendus cum. Voluptatem distinctio beatae dolor? Corporis labore repellat laudantium, tempore non animi?</p>
+      </div>
+    </>
   );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,19 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem eius quae, aliquid sapiente enim fuga! At quisquam dolor accusamus pariatur dignissimos voluptatibus, fuga ipsa molestias laboriosam laborum nostrum consectetur culpa.</p>
-      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem eius quae, aliquid sapiente enim fuga! At quisquam dolor accusamus pariatur dignissimos voluptatibus, fuga ipsa molestias laboriosam laborum nostrum consectetur culpa.</p>
-      <Link href="/ninjas">
-        <a class={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem eius quae, aliquid sapiente enim fuga! At quisquam dolor accusamus pariatur dignissimos voluptatibus, fuga ipsa molestias laboriosam laborum nostrum consectetur culpa.</p>
+        <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem eius quae, aliquid sapiente enim fuga! At quisquam dolor accusamus pariatur dignissimos voluptatibus, fuga ipsa molestias laboriosam laborum nostrum consectetur culpa.</p>
+        <Link href="/ninjas">
+          <a class={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
